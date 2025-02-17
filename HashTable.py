d9163f14600641eefc3b705a31126d754d8a8d49
jPippen89 <JOSHPIPPEN89@ICLOUD.COM>
2025-02-17 10:46:39 -0600

    Altered HashTable

---
+++ HashTable.py
@@ -0,0 +1,54 @@
+from LinkList import SLL, Node
+
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.arr=[SLL() for i in range(self.size)]
+
+    def __get_hash(self, key):
+        index = 0
+        for char in key:
+            index += ord(char)
+        return index % self.size
+
+    def put(self, key, data):
+        primary_index = self.__get_hash(key)
+        if self.arr[primary_index] is None:
+            self.arr[primary_index] = data
+            return True
+        else:
+            secondary_index = self.__get_second_hash(key)
+            if self.arr[secondary_index] is None:
+                self.arr[secondary_index] = data
+                return True
+            else:
+                print("Hash Table Collision, Data NOT Saved")
+                return False
+
+    def putSLL(self, key, data):
+        primary_index = self.__get_hash(key)
+        self.arr[primary_index].prepend(data)
+
+    def removeSLL(self, key):
+        index = self.__get_hash(key)
+        self.arr[index].delete(key)
+        return True
+
+    def get(self, key):
+        index = self.__get_hash(key)
+        return self.arr[index]
+
+
+
+    def __str__(self):
+        result = ""
+        for data in self.arr:
+            if data is not None:
+                result += f"{str(data)}\n"
+            return result
+
+    # def __get_second_hash(self, key):
+    #     index = 0
+    #     for char in key:
+    #         index += ord(char)
+    #     return (index * 17 + 1) % self.size
